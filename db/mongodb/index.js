78f75386741ba7d0e4bd25c1dedd0951c2284464
David Weinzimmer <dweinzimmer@gmail.com>
2018-10-21 16:30:05 -0400

    Write database methods

--- db/mongodb/index.js
+++ db/mongodb/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 if(process.env.MONGODB_URI){
   mongoose.connect(process.env.MONGODB_URI)
-} else{
+} else {
   mongoose.connect('mongodb://localhost:27017/badmovies', { useNewUrlParser: true });
 }
 
--- server/models/movieModel.js
+++ server/models/movieModel.js
@@ -2,8 +2,59 @@
 //For SQL
 // const sqlDb = require('../../db/sql');
 //For Mongo
-const mongoDb = require('../../db/mongodb')
+const db = require('../../db/mongodb')
+
+var movieSchema = new mongoose.Schema({
+  title: String,
+  id: {
+    type: Number,
+    unique: true
+  },
+  vote_average: Number,
+  vote_count: Number,
+  poster_path: String,
+  release_date: String,
+  popularity: Number
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+
+let addToDB = (movieFromClient) => {
+  let newMovie = new Movie(movieFromClient);
+  newMovie.save((err) => {
+    console.error(err);
+  });
+
+  // ANOTHER WAY...
+  // Movie.create(movieFromClient, (err, movie) => {
+  //   if (err) {
+  //     console.error(err)
+  //   }
+  // })
+}
+
+let deleteFromDB = (movieFromClient) => {
+  Movie.findOneAndDelete({id: movieFromClient.id}, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  })
+}
+
+let getFromDB = () => {
+  Movie.find({}, (err, docs) => {
+    if (err) {
+      console.error(err);
+    } else {
+      return docs;
+    }
+  })
+}
+
 
 module.exports = {
-
+  addToDB,
+  deleteFromDB,
+  getFromDB
 }
